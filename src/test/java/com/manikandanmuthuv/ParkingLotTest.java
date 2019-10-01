35962ff6df0b8f3d7527009d307d562f8aab0eb9
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-10-01 20:26:56 +0530

    failing test to allocate car in slot

--- src/test/java/com/manikandanmuthuv/ParkingLotTest.java
+++ src/test/java/com/manikandanmuthuv/ParkingLotTest.java
@@ -8,15 +8,33 @@ import static org.hamcrest.CoreMatchers.*;
 
 public class ParkingLotTest {
 
+    private int numberOfSlots = 6;
+
     @Test
     public void CreateParkingLotWithSlots() {
         //Arrange
-        int numberOfSlots = 6;
         String expectedParkingLot = "Created a parking lot with " + numberOfSlots + " slots";
         ParkingLot parkingLot = new ParkingLot();
         //Act
         String actualParkingLot = parkingLot.Create(numberOfSlots);
         //Assert
         assertThat(expectedParkingLot, is(actualParkingLot));
+    }
+    
+    @Test
+    public void allocatCarInSlot() {
+        //Arrange
+        ParkingLot ParkingLot = new ParkingLot();
+        ParkingLot.Create(numberOfSlots);
+        String regNumber = "KA-01-HH-1234";
+        String color = "White";
+        Car car = new Car(regNumber, color);
+
+        //Act
+        String result = ParkingLot.AllocateSlot(car);
+
+        //Assert
+        assertThat("Allocated slot number: 1", is(result));
+
     }
 }
