48f1dc66ad986bcbe639e8aa42757a512f0fffed
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-10-01 20:49:37 +0530

    code to leave slot and made the failing test pass

--- src/main/java/com/manikandanmuthuv/ParkingLot.java
+++ src/main/java/com/manikandanmuthuv/ParkingLot.java
@@ -7,7 +7,7 @@ public class ParkingLot {
 
     private Map<Integer, Car> slot = new HashMap<>();
     private int totalSlots;
-    
+
     public ParkingLot() {
     }
 
@@ -21,15 +21,24 @@ public class ParkingLot {
 
     public String AllocateSlot(Car car) {
         int availableSlotNumber = 0;
-            
+
         for (int slotNumberKey = 0; slotNumberKey < slot.size(); slotNumberKey++) {
             if (slot.get(slotNumberKey).getRegNumber() == null) {
                 // add 1 to get the right slot number as index starts from 0
-                availableSlotNumber = slotNumberKey + 1; 
+                availableSlotNumber = slotNumberKey + 1;
                 slot.put(slotNumberKey, car);
                 break;
-            }                        
+            }
         }
         return "Allocated slot number: " + availableSlotNumber;
+    }
+
+    public String LeaveSlot(int fromSlotNumber) {
+        // substract 1 to get the right slot number as index starts from 0
+        if (slot.get(fromSlotNumber - 1).getRegNumber() != null) {
+            slot.put(fromSlotNumber - 1, new Car());
+        }
+        return "Slot number " + fromSlotNumber + " is free";
+
     }
 }
